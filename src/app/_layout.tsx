7ba3848a33e7097999b9ea8891386932a328c78a
Roberto Cirera Procópio Jr <35637328+rocpjunior@users.noreply.github.com>
2026-09-18 11:59:09 -0300

    some changes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function App(){
+    return(
+        <Stack>
+            <Stack.Screen name="index"/>
+        </Stack>
+    )
+ 
 }
